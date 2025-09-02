ce06a800d24edf9500c4930fdc5f0a6452260968
Kristian <kristianism@pm.me>
2025-09-02 22:29:42 +0800

    Add HyperYield logo

--- app/page.tsx
+++ app/page.tsx
@@ -7,17 +7,17 @@ export default function Home() {
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-center">
         <Image
-          className="dark:invert animate-ping"
-          src="/hl-symbol-mint-green.svg"
-          alt="HyperLiquid logo"
-          width={180}
-          height={38}
+          className="invert dark:invert-0"
+          src="/logo.svg"
+          alt="HyperYield logo"
+          width={480}
+          height={50}
           priority
         />
         <Image
           className="dark:invert"
           src="/powered-by-hl.svg"
-          alt="Next.js logo"
+          alt="Powered by HyperLiquid"
           width={300}
           height={38}
           priority
@@ -25,7 +25,7 @@ export default function Home() {
         <ul className="font-mono list-inside list-disc text-sm/6 text-center sm:text-left">
           <li className="mb-2 tracking-[-.01em]">
             <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              HyperEVM Farm: Because Slow Yields Are for Normies.
+              HyperYield: Because Slow Yields Are for Normies.
             </code>
           </li>
           <li className="tracking-[-.01em]">
@@ -35,7 +35,7 @@ export default function Home() {
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
+            className="animate-pulse rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
             href="/dashboard"
             rel="noopener noreferrer"
           >
@@ -46,13 +46,6 @@ export default function Home() {
               width={20}
               height={20}
             />
-            Get Hyper
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="/dashboard"
-            rel="noopener noreferrer"
-          >
             Catch the Flow
           </a>
         </div>
