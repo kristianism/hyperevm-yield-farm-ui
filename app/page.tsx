ed0cc154b2db4f817884400ba4547e91ed83f3ec
Kristian <kristianism@pm.me>
2025-09-03 00:08:30 +0800

    Update logo

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-center">
         <Image
-          className="dark:invert"
+          className="invert dark:invert-0"
           src="/logo.svg"
           alt="HyperYield logo"
           width={150}
